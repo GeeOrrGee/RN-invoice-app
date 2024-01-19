702d2fcad49b2762eef3430e14e775144c125d67
macbook <gioxezz@gmail.com>
2024-01-19 15:30:12 +0400

    theme initialized

---
+++ app/theme.ts
@@ -0,0 +1,28 @@
+export const theme = {
+  colors: {
+    primary: {
+      purple100: "#9277ff",
+      purple200: "#7c5dfa",
+    },
+    secondary: {
+      100: "#DFE3FA",
+      400: "#7E88C3",
+      600: "#888EB0",
+    },
+    darkBlue: {
+      100: "#252945",
+      200: "#1E2139",
+    },
+    black: {
+      100: "#0c0e16",
+      200: "#141625",
+    },
+    red: {
+      100: "#9277FF",
+      200: "#EC5757",
+    },
+    whiteBg: "#f8f8f8",
+  },
+};
+
+//TODO Refactor this as per theme mode color palette
