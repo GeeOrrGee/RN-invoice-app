a367d01ba2b14735f7aa4eeeb4f7b070f4f0e690
giorgadzesab@gmail.com <giorgadzesab@gmail.com>
2024-01-20 01:14:24 +0400

    added theme colors

---
+++ app/theme.js
@@ -0,0 +1,20 @@
+export default {
+    light : {
+        background: '#FFFFFF',
+        text: '#000000',
+        primary: '#3498db',
+        tabIconDefault : '#ccc',
+        buttonBackground: '#3498db',
+        buttonTextColor: '#FFFFFF',
+        border: '#bdc3c7',
+    },
+    dark : {
+        background: '#2c3e50',
+        text: '#FFFFFF',
+        primary: '#2980b9',
+        tabIconDefault: '#ccc',
+        buttonBackground: '#2980b9',
+        buttonTextColor: '#FFFFFF',
+        border: '#34495e',
+    }
+};
