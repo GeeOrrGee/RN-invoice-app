45b4fd8f3985de60b075377ffa2cf42eeca3d868
macbook <gioxezz@gmail.com>
2024-01-23 00:19:00 +0400

    add default header for the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,8 @@ import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import { useColorScheme } from "react-native";
-import { colors, components } from "./theme";
-import { ThemeProvider, createTheme } from "@rneui/themed";
+import { colors, components, palette } from "./theme";
+import { ThemeProvider, createTheme, useTheme } from "@rneui/themed";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -52,10 +52,21 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
+  const headerConfig = {
+    screenOptions: {},
+  };
   return (
     <ThemeProvider theme={theme}>
-      <Stack>
-        <Stack.Screen name="(homepage)" options={{ headerShown: false }} />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: palette.darkBlue,
+          },
+          headerTintColor: palette.primaryLight,
+          headerTitle: "Universal header in the app",
+        }}
+      >
+        <Stack.Screen name="(homepage)" />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
     </ThemeProvider>
